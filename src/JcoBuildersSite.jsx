d72bc8a78905d52783b254381c27509f7d599cb2
Dustenenen <qjdesalvaloza@tip.edu.ph>
2025-10-26 19:23:42 +0800

    updated

--- src/JcoBuildersSite.jsx
+++ src/JcoBuildersSite.jsx
@@ -397,7 +397,7 @@ export default function JcoBuildersSite() {
             </a>
           </p>
           <p className="text-gray-800 font-medium mt-2">
-            Phone: +63 94-4966-729
+            Phone: +63 949-4966-729
           </p>
         </div>
       </section>
